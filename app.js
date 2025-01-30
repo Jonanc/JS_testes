22f9eece5dbd2bca9c3f4fb7c97f70ef9168f50f
Jorge Fernando <jorgefernandonc@gmail.com>
2025-01-30 16:30:38 -0300

    melhoras no codigo html e JS

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ function exibirTextoNaTela(tag, informacao){
 
 function exibirTexto() {
     exibirTextoNaTela('h1','sistema de<br>Boas vindas<br>ao usuario :) '); //Quando eu coloquei h1 ele substitui a "tag" no paramaetro e o conteudo substitui a informacao
-    exibirTextoNaTela('p1','Digite seu nome:');
-    exibirTextoNaTelaId('paragrafo2', 'Digite sua idade:');
-    exibirTextoNaTelaId('paragrafo3', 'Digite Sua Cidade');
-    exibirTextoNaTelaId('paragrafo4', 'Digite Seu Estado');
-    exibirTextoNaTelaId('paragrafo5', 'Digite Seu Cargo De Interesse');
-    exibirTextoNaTelaId('paragrafo6', 'Digite seu CPF');
-    exibirTextoNaTelaId('paragrafo7', 'digite sua Escolaridade');
+    exibirTextoNaTelaId('nome','Digite seu nome:');
+    exibirTextoNaTelaId('idade', 'Digite sua idade:');
+    exibirTextoNaTelaId('cidade', 'Digite Sua Cidade');
+    exibirTextoNaTelaId('estado', 'Digite Seu Estado');
+    exibirTextoNaTelaId('cargoDeInteresse', 'Digite Seu Cargo De Interesse');
+    exibirTextoNaTelaId('CPF', 'Digite seu CPF');
+    exibirTextoNaTelaId('escolaridade', 'Digite Sua Escolaridade');
 }
 
 function exibirTextoNaTelaId(id, informacaoPorId){
@@ -21,13 +21,43 @@ function exibirTextoNaTelaId(id, informacaoPorId){
 
 exibirTexto();
 
-function nome () {
-   return document.querySelector('input').value;
+function funcaoNome () {
+   return document.getElementById('inputNome').value;
+}
+
+function funcaoIdade() {
+    return document.getElementById('inputIdade').value;
+}
+
+function funcaoCidade() {
+    return document.getElementById('inputCidade').value;
+}
+
+function funcaoEstado() {
+    return document.getElementById('inputEstado').value;
+}
+
+function funcaoCargo() {
+    return document.getElementById('inputCargoDeInteresse').value;
+}
+
+function funcaoCPF() {
+    return document.getElementById('inputCPF').value;
+}
+
+function funcaoEscolaridade() {
+    return document.getElementById('inputEscolaridade').value;
 }
 
 function chute() {
-    let nomeUsuario = nome();
-    exibirTextoNaTela('p1', `Seja bem-vindo usuario: ${nomeUsuario}`);
+    let nomeUsuario = funcaoNome();
+    let idadeUsuario = funcaoIdade();
+    let cidadeUsuario = funcaoCidade();
+    let estadoUsuario = funcaoEstado();
+    let cargoUsuario = funcaoCargo();
+    let CPFUsuario = funcaoCPF();
+    let escolaridadeUsuario = funcaoEscolaridade();
+    //exibirTextoNaTela('p1', `Seja bem-vindo usuario: ${idadeUsuario}`);
 }
 
 function gerarNumeroAleatorio() {
